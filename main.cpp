fcf61f77524ab91a6e3eddb041f15829dae48498
zephyrhunt <1534150771@qq.com>
2022-08-08 09:46:50 +0800

    add v3

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ int main()
 	std::cout << "Hello, World!" << std::endl;
 	std::cout << "Hello, Version1!" << std::endl;
 	std::cout << "Hello, Version2!" << std::endl;
+	std::cout << "Hello, Version3!" << std::endl;
 
+	std::cout << "Merge Conflicts" << std::endl;
 	return 0;
 }
