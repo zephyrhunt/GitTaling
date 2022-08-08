514043eb56cb6a0a8b764c153fc47a53f1e0e883
zephyrhunt <1534150771@qq.com>
2022-08-08 09:47:05 +0800

    add v4

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main()
 	std::cout << "Hello, Version1!" << std::endl;
 	std::cout << "Hello, Version2!" << std::endl;
 	std::cout << "Hello, Version3!" << std::endl;
+	std::cout << "Hello, Version4!" << std::endl;
 
 	std::cout << "Merge Conflicts" << std::endl;
 	return 0;
